73aa4402f42a0e6558ff0a94d2420db652fcbd3c
jayesh-kaza <jayeshkaza8998@gmail.com>
2020-08-29 14:27:30 +0530

    Updates

---
+++ precache-manifest.a90767791509535e0abcb9fe2b18b8b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc4d1fcadc778584c459ab5e6e8c0bd",
+    "url": "/Going-back-to-React/index.html"
+  },
+  {
+    "revision": "893416c1cac2f77d7c34",
+    "url": "/Going-back-to-React/static/css/main.141d7862.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/Going-back-to-React/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Going-back-to-React/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "893416c1cac2f77d7c34",
+    "url": "/Going-back-to-React/static/js/main.c6277b49.chunk.js"
+  },
+  {
+    "revision": "0bba645f69ef2d3d854c",
+    "url": "/Going-back-to-React/static/js/runtime-main.cca41829.js"
+  }
+]);
